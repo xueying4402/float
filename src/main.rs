f5626965da7bab52a65d7a0dcad274e3693eb2f3
xueying4402 <1120859481@qq.com>
2021-12-10 19:37:01 +0800

    wasmtime and wasmer using f64 and f32

--- src/main.rs
+++ src/main.rs
@@ -1,81 +1,37 @@
-//! Wasmer will let you easily run Wasm module in a Rust host.
-//!
-//! This example illustrates the basics of using Wasmer through a "Hello World"-like project:
-//!
-//!   1. How to load a Wasm modules as bytes
-//!   2. How to compile the module
-//!   3. How to create an instance of the module
-//!
-//! You can run the example directly by executing in Wasmer root:
-//!
-//! ```shell
-//! cargo run --example instance --release --features "cranelift"
-//! ```
-//!
-//! Ready?
+use anyhow::Result;
+use wasmtime::*;
 
-use wasmer::{imports, wat2wasm, Instance, Module, Store};
-use wasmer_compiler_cranelift::Cranelift;
-use wasmer_engine_universal::Universal;
-
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // Let's declare the Wasm module.
-    //
-    // We are using the text representation of the module here but you can also load `.wasm`
-    // files using the `include_bytes!` macro.
-    let wasm_bytes = wat2wasm(
-        br#"
+fn main() -> Result<()> {
+    // Modules can be compiled through either the text or binary format
+    let engine = Engine::default();
+    let wat = r#"
         (module
-            (type $add_one_t (func (param f32) (result f32)))
-            (func $add_one_f (type $add_one_t)  (param $value f32) (result f32)
-              local.get $value
+            (func $add_one_f (result f32)
+              f32.const 0.0
               f32.const 0.0
               f32.div)
             (export "add_one" (func $add_one_f))
-          )"#,
-    )?;
+          )"#;
+    let module = Module::new(&engine, wat)?;
 
-    // Create a Store.
-    // Note that we don't need to specify the engine/compiler if we want to use
-    // the default provided by Wasmer.
-    // You can use `Store::default()` for that.
-    let store = Store::new(&Universal::new(Cranelift::default()).engine());
+    // All wasm objects operate within the context of a "store". Each
+    // `Store` has a type parameter to store host-specific data, which in
+    // this case we're using `4` for.
+    let mut store = Store::new(&engine, 4);
+    // let host_hello = Func::wrap(&mut store, |caller: Caller<'_, u32>, param: i32| {
+    //     println!("Got {} from WebAssembly", param);
+    //     println!("my host state is: {}", caller.data());
+    // });
 
-    println!("Compiling module...");
-    // Let's compile the Wasm module.
-    let module = Module::new(&store, wasm_bytes)?;
+    // Instantiation of a module requires specifying its imports and then
+    // afterwards we can fetch exports by name, as well as asserting the
+    // type signature of the function with `get_typed_func`.
+    let instance = Instance::new(&mut store, &module, &[])?;
+    let hello = instance.get_typed_func::<(), f32, _>(&mut store, "add_one")?;
 
-    // Create an empty import object.
-    let import_object = imports! {};
-
-    println!("Instantiating module...");
-    // Let's instantiate the Wasm module.
-    let instance = Instance::new(&module, &import_object)?;
-
-    // We now have an instance ready to be used.
-    //
-    // From an `Instance` we can retrieve any exported entities.
-    // Each of these entities is covered in others examples.
-    //
-    // Here we are retrieving the exported function. We won't go into details here
-    // as the main focus of this example is to show how to create an instance out
-    // of a Wasm module and have basic interactions with it.
-    let add_one = instance
-        .exports
-        .get_function("add_one")?
-        .native::<f32, f32>()?;
-
-    println!("Calling `add_one` function...");
-    let result = add_one.call(0.0)?;
-    println!("{:?}", result.to_be_bytes());
-
-    println!("Results of `add_one`: {:?}", result);
-    assert_eq!(result, f32::NAN);
+    // And finally we can call the wasm!
+    let ans = hello.call(&mut store, ())?;
+    println!("x86 wasmtime f32: {:?}", ans.to_be_bytes());
 
     Ok(())
-}
-
-#[test]
-fn test_exported_function() -> Result<(), Box<dyn std::error::Error>> {
-    main()
 }
